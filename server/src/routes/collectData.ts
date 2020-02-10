1588a195bbd901cfa6ca442ea68af72396e06bd4
yonikim <yonikim@wisenut.co.kr>
2020-02-10 14:53:57 +0900

    server - 수집데이터 확인 sql문 수정

--- server/src/routes/collectData.ts
+++ server/src/routes/collectData.ts
@@ -7,80 +7,95 @@ const router = express.Router();
 
 router.post('/:kind', async (req, res) => {
   const kind = req.params.kind;
+  var always_yn = '';
+  if (kind === 'always') {
+    always_yn = 'Y';
+  } else if (kind === 'retroactive') {
+    always_yn = 'N';
+  }
 
   try {
     const manager = getConnectionManager().get('verify');
-    const repository = manager.getRepository(CollectData).createQueryBuilder();
-
-    const { customer_id, channel } = req.body;
+    const repository = manager
+      .getRepository(CollectData)
+      .createQueryBuilder('d');
 
     var datas = [];
-    if (kind === 'always') {
-      if (customer_id === undefined && channel === undefined) {
-        datas = await repository
-          .where({ always_yn: 'Y' })
-          .groupBy('channel')
-          .orderBy({ customer_id: 'ASC' })
-          .getMany();
-      } else {
-        datas = await repository
-          .where({
-            always_yn: 'Y',
-            customer_id: customer_id,
-            channel: channel,
-          })
-          .orderBy({ doc_datetime: 'DESC' })
-          .getMany();
-      }
-    } else if (kind === 'retroactive') {
-      datas = await repository
-        .where({ always_yn: 'N' })
-        .groupBy('channel')
-        .orderBy({ customer_id: 'ASC' })
-        .getMany();
-    }
+
+    datas = await repository
+      .where('always_yn = :always_yn', { always_yn })
+      .groupBy('channel')
+      .orderBy({ customer_id: 'ASC' })
+      .getMany();
+
     return res.json({ data: datas });
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }
 });
 
-router.post(
-  '/:kind/:customer_id/:collect_type/:doc_datetime',
-  async (req, res) => {
-    const kind = req.params.kind;
+router.post('/:kind/getCustomers', async (req, res) => {
+  const kind = req.params.kind;
+  var always_yn = '';
+  if (kind === 'always') {
+    always_yn = 'Y';
+  } else if (kind === 'retroactive') {
+    always_yn = 'N';
+  }
 
-    try {
-      const manager = getConnectionManager().get('verify');
-      const repository = manager.getRepository(CollectData);
+  try {
+    const manager = getConnectionManager().get('verify');
+    const repository = manager
+      .getRepository(CollectData)
+      .createQueryBuilder('d');
 
-      const customer_id = req.params.customer_id;
-      const collect_type = req.params.collect_type;
-      const doc_datetime = req.params.doc_datetime;
+    var datas = [];
+    datas = await repository
+      .select(['DISTINCT d.customer_id'])
+      .where('always_yn = :always_yn', { always_yn })
+      .orderBy({ customer_id: 'ASC' })
+      .getRawMany();
 
-      var datas = [];
-      if (kind === 'always') {
-        datas = await repository.find({
-          always_yn: 'Y',
-          ...(customer_id !== "''" && { customer_id }),
-          ...(collect_type !== "''" && { collect_type }),
-          ...(doc_datetime !== "''" && { doc_datetime }),
-        });
-      } else if (kind === 'retroactive') {
-        datas = await repository.find({
-          always_yn: 'N',
-          ...(customer_id !== "''" && { customer_id }),
-          ...(collect_type !== "''" && { collect_type }),
-          ...(doc_datetime !== "''" && { doc_datetime }),
-        });
-      }
+    return res.json({ data: datas });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
 
-      return res.json({ data: datas });
-    } catch (e) {
-      res.status(404).json({ message: e.message });
-      return res.status(500).json({ message: e.message });
-    }
-  },
-);
+router.post('/:kind/:customer_id/:channel', async (req, res) => {
+  const kind = req.params.kind;
+  var always_yn = '';
+  if (kind === 'always') {
+    always_yn = 'Y';
+  } else if (kind === 'retroactive') {
+    always_yn = 'N';
+  }
+
+  try {
+    const manager = getConnectionManager().get('verify');
+    const repository = manager.getRepository(CollectData);
+
+    const customer_id = req.params.customer_id;
+    const channel = req.params.channel;
+
+    var datas = [];
+
+    datas = await repository.find({
+      where: {
+        always_yn: always_yn,
+        customer_id: customer_id,
+        channel: channel,
+      },
+      order: {
+        doc_datetime: 'DESC',
+      },
+    });
+
+    return res.json({ data: datas });
+  } catch (e) {
+    res.status(404).json({ message: e.message });
+    return res.status(500).json({ message: e.message });
+  }
+});
 
 export default router;
