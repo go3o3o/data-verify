1393d6f33063504bb407a41e613befa83aa13d5e
yonikim <yonikim@wisenut.co.kr>
2020-02-10 14:47:14 +0900

    client - 상시 수집데이터 확인 페이지

---
+++ client/src/pages/CollectAlways/FilterDocDatetime/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Select, Avatar, Input } from 'antd';
+import 'antd/dist/antd.css';
+
+export const FilterDocDatetime = ({ filterBy, data, ...props }) => {
+  const onClick = value => {
+    filterBy('filterDocDatetime', 'doc_datetime', value, data);
+  };
+
+  return (
+    <>
+      <Input.Group compact>
+        <Avatar
+          shape="square"
+          size="large"
+          style={{
+            color: '#353A40',
+            fontSize: 15,
+            width: 65,
+          }}
+        >
+          수집일자
+        </Avatar>
+        <Select
+          id="status"
+          size="large"
+          style={{ width: '230px', marginRight: '40px' }}
+          onChange={onClick}
+        >
+          <Select.Option key="status" value="all">
+            전체
+          </Select.Option>
+          <Select.Option key="status" value="CRS001">
+            7일전
+          </Select.Option>
+          <Select.Option key="status" value="CRS002">
+            6일전
+          </Select.Option>
+          <Select.Option key="status" value="CRS002">
+            6일전
+          </Select.Option>
+          <Select.Option key="status" value="CRS002">
+            6일전
+          </Select.Option>
+          <Select.Option key="status" value="CRS002">
+            6일전
+          </Select.Option>
+          <Select.Option key="status" value="CRS002">
+            6일전
+          </Select.Option>
+        </Select>
+      </Input.Group>
+    </>
+  );
+};
--- client/src/pages/CollectAlways/index.tsx
+++ client/src/pages/CollectAlways/index.tsx
@@ -1,148 +1,250 @@
 import React, { Component } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import Async from 'react-async';
 import { inject, observer } from 'mobx-react';
 
-import {
-  Col,
-  Row,
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-} from 'reactstrap';
-import { Table } from 'antd';
+import { Table, Form, Spin, Button, Icon, Modal } from 'antd';
 import 'antd/dist/antd.css';
 
 import { STORES } from '~constants';
 
-import MenuBar from '~components/MenuBar';
 import VerifyStore from '~stores/verify/VerifyStore';
-import DetailAlways from '~pages/CollectAlways/DetailAlways';
+
+import MenuBar from '~components/MenuBar';
+import { FilterCustomer } from './FilterCustomer';
+import { FilterCollectType } from './FilterCollectType';
+import { FilterDocDatetime } from './FilterDocDatetime';
 
 type InjectedProps = {
   [STORES.VERIFY_STORE]: VerifyStore;
-} & RouteComponentProps<{
-  customer_id: string;
-  collect_type: string;
-  doc_datetime: string;
-}>;
+};
 
-class CollectAlways extends Component<InjectedProps & RouteComponentProps> {
+class CollectAlways extends Component<InjectedProps> {
   constructor(props: any) {
     super(props);
+    this.state = { alwaysData: undefined, visible: false, viewData: undefined };
+    this.props[STORES.VERIFY_STORE].alwaysData();
+
+    this.showModal = this.showModal.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.loadAlwaysData = this.loadAlwaysData.bind(this);
+    this.loadAlwaysDetailData = this.loadAlwaysDetailData.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
-  componentWillMount(): void {
-    const customerId = this.props.match.params.customer_id;
-    const collectType = this.props.match.params.collect_type;
-    const docDatetime = this.props.match.params.doc_datetime;
+  showModal = () => {
+    this.setState({ visible: true });
+  };
 
-    if (
-      customerId !== undefined ||
-      collectType !== undefined ||
-      docDatetime !== undefined
-    ) {
-      this.props[STORES.VERIFY_STORE].searchAlwaysData(
-        customerId,
-        collectType,
-        docDatetime,
-      );
-    } else {
-      this.props[STORES.VERIFY_STORE].alwaysData();
+  handleOk = e => {
+    this.setState({ visible: false });
+  };
+  handleCancel = e => {
+    this.setState({ visible: false });
+  };
+
+  loadAlwaysData(data): object[] {
+    if (this.state['alwaysData'] === undefined) {
+      data.map(d => {
+        d['filterCustomer'] = true;
+        d['filterCollectType'] = true;
+        d['filterDocDatetime'] = true;
+        if (d.collect_type == 0) {
+          d['collect_type_nm'] = '디맵수집기';
+        }
+        if (d.collect_type == 1) {
+          d['collect_type_nm'] = '신규수집기';
+        }
+      });
+      return data;
     }
+    return this.state['alwaysData'];
   }
+
+  loadAlwaysDetailData = (customer_id: string, channel: string) => {
+    return this.props[STORES.VERIFY_STORE]
+      .alwaysDetailData(customer_id, channel)
+      .then(res => {
+        return res.data;
+      });
+  };
+
+  handleFilter = (name, column, value, data) => {
+    var alwaysData: object[] = [];
+    if (value === 'all') {
+      alwaysData = data.filter(d => {
+        d[name] = true;
+        return (
+          d['filterCustomer'] === true &&
+          d['filterCollectType'] === true &&
+          d['filterDocDatetime'] === true
+        );
+      });
+    } else {
+      alwaysData = data.filter(d => {
+        if (d[column] === value) {
+          d[name] = true;
+        } else {
+          d[name] = false;
+        }
+        return (
+          d['filterCustomer'] === true &&
+          d['filterCollectType'] === true &&
+          d['filterDocDatetime'] === true
+        );
+      });
+    }
+    this.setState({ alwaysData: alwaysData });
+  };
 
   render() {
+    var { data } = this.props[STORES.VERIFY_STORE];
+    var { customers } = this.props[STORES.VERIFY_STORE];
+
+    const expandedRowRender = (e: any) => {
+      const customer_id = e.customer_id;
+      const channel = e.channel;
+
+      const columns = [
+        {
+          title: '수집일자',
+          dataIndex: 'doc_datetime',
+          key: 'doc_datetime',
+          ellipsis: true,
+        },
+        {
+          title: '수집채널',
+          dataIndex: 'channel',
+          key: 'channel',
+          ellipsis: true,
+        },
+        {
+          title: '제목',
+          dataIndex: 'doc_title',
+          key: 'doc_title',
+          ellipsis: true,
+        },
+        {
+          title: '내용',
+          dataIndex: 'doc_content',
+          key: 'doc_content',
+          ellipsis: true,
+        },
+        { title: 'URL', dataIndex: 'doc_url', key: 'doc_url', ellipsis: true },
+        {
+          title: '첨부여부',
+          dataIndex: 'attach_yn',
+          key: 'attach_yn',
+          ellipsis: true,
+        },
+        { title: '상세보기', dataIndex: 'detail', key: 'detail' },
+      ];
+
+      const onClick = data => {
+        this.setState({ viewData: data });
+        this.showModal();
+      };
+
+      return (
+        <Async promise={this.loadAlwaysDetailData(customer_id, channel)}>
+          <Async.Loading>
+            <div style={{ position: 'absolute', top: '50%', left: '50%' }}>
+              <Spin size="large" />
+            </div>
+          </Async.Loading>
+          <Async.Resolved>
+            {data => {
+              data['data'].map(d => {
+                d['detail'] = (
+                  <>
+                    <Button
+                      type="link"
+                      shape="circle"
+                      size="large"
+                      icon="file"
+                      onClick={() => onClick(d)}
+                    />
+                  </>
+                );
+              });
+
+              return <Table columns={columns} dataSource={data['data']} />;
+            }}
+          </Async.Resolved>
+        </Async>
+      );
+    };
+
     const columns = [
-      { title: '고객명', dataIndex: 'customer_id', key: 'customer_id' },
-      { title: '수집채널', dataIndex: 'channel', key: 'channel' },
+      {
+        title: '고객명',
+        dataIndex: 'customer_id',
+        key: 'customer_id',
+        width: '20%',
+      },
+      { title: '수집채널', dataIndex: 'channel', key: 'channel', width: '60%' },
       {
         title: '수집방법',
         dataIndex: 'collect_type_nm',
         key: 'collect_type_nm',
+        width: '20%',
       },
     ];
-
-    const { data } = this.props[STORES.VERIFY_STORE];
-    data.map(d => {
-      if (d.collect_type == 0) {
-        d['collect_type_nm'] = '디맵수집기';
-      }
-      if (d.collect_type == 1) {
-        d['collect_type_nm'] = '신규수집기';
-      }
-    });
-
-    const { verifyStore } = this.props;
-
-    const expandedRowRender = (e: any) => {
-      console.log('expandedRowRender');
-
-      const customer_id = e.customer_id;
-      const channel = e.channel;
-
-      // ️verifyStore에 변수 대입 먼저 해준 후에 <DetailAlways> 호출⭐️
-      verifyStore.setCustomerId(customer_id);
-      verifyStore.setChannel(channel);
-      verifyStore.alwaysDetailData();
-
-      return (
-        <DetailAlways
-          verifyStore={verifyStore}
-          customer_id={customer_id}
-          channel={channel}
-        />
-      );
-    };
 
     return (
       <>
         <MenuBar subMenu="" />
         <div style={{ textAlign: 'center' }}>
-          <div style={{ width: '90%', display: 'inline-block' }}>
-            <Form style={{ padding: 30 }}>
-              <Row form>
-                <Col>
-                  <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>고객명</InputGroupText>
-                    </InputGroupAddon>
-                    <Input type="select" name="customerId" id="customerId" />
-                  </InputGroup>
-                </Col>
-                <Col>
-                  <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>수집방법</InputGroupText>
-                    </InputGroupAddon>
-                    <Input type="select" name="collectType" id="collectType" />
-                  </InputGroup>
-                </Col>
-                <Col>
-                  <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>수집일자</InputGroupText>
-                    </InputGroupAddon>
-                  </InputGroup>
-                </Col>
-                <FormGroup check row>
-                  <Col>
-                    <Button>검색</Button>
-                  </Col>
-                </FormGroup>
-              </Row>
+          <div style={{ width: '90%', display: 'inline-block', marginTop: 40 }}>
+            <Form
+              layout="inline"
+              style={{ textAlign: 'left', marginBottom: 30 }}
+            >
+              <Form.Item>
+                <FilterCustomer
+                  filterBy={this.handleFilter}
+                  data={data}
+                  customers={customers}
+                />
+              </Form.Item>
+              <Form.Item>
+                <FilterCollectType filterBy={this.handleFilter} data={data} />
+              </Form.Item>
+              <Form.Item>
+                {/* <FilterDocDatetime
+                  filterBy={this.handleFilter}
+                  data={data}
+                /> */}
+              </Form.Item>
             </Form>
 
             <Table
               className="components-table-demo-nested"
               columns={columns}
+              dataSource={this.loadAlwaysData(data)}
               expandedRowRender={expandedRowRender}
-              dataSource={data}
             />
+            <Modal
+              title={
+                this.state['visible'] ? (
+                  <p>
+                    {`${this.state['viewData'].customer_id} >
+                      ${this.state['viewData'].channel}`}
+                  </p>
+                ) : (
+                  <p>No Data</p>
+                )
+              }
+              visible={this.state['visible']}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              {this.state['visible'] ? (
+                <p>{this.state['viewData'].doc_content}</p>
+              ) : (
+                <p>No Data</p>
+              )}
+            </Modal>
           </div>
         </div>
       </>
